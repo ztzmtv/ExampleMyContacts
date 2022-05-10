df457da2da5e24a6bbdd86164a3a8cba47f8ed73
azmetoff <azmetoff@gmail.com>
2022-05-10 15:01:45 +0300

    request contacts, permission denied

---
+++ app/src/main/java/com/aztown/mycontacts/Contact.kt
@@ -0,0 +1,6 @@
+package com.aztown.mycontacts
+
+data class Contact(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/com/aztown/mycontacts/MainActivity.kt
+++ app/src/main/java/com/aztown/mycontacts/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.aztown.mycontacts
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+        if (permissionGranted) {
+            requestContacts()
+        } else {
+            Log.d("MainActivity_TAG", "Permission denied")
+        }
+    }
+
+    private fun requestContacts() {
+        thread {
+            val cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null
+            )
+            while (cursor?.moveToNext() == true) {
+                val id = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+                )
+                val name = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
+                )
+                val contact = Contact(id, name)
+                Log.d("MainActivity_TAG", "$contact")
+            }
+            cursor?.close()
+        }
     }
 }
