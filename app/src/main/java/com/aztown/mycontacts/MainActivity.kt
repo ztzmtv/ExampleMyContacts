169ed6fae2aeb509db6c75d1d5993d0e1a6de518
azmetoff <azmetoff@gmail.com>
2022-05-10 15:41:19 +0300

    request contacts and set in to Log

--- app/src/main/java/com/aztown/mycontacts/MainActivity.kt
+++ app/src/main/java/com/aztown/mycontacts/MainActivity.kt
@@ -20,8 +20,32 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("MainActivity_TAG", "Permission denied")
+            requestPermission()
         }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("MainActivity_TAG", "Permission Denied")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun requestContacts() {
@@ -42,5 +66,9 @@ class MainActivity : AppCompatActivity() {
             }
             cursor?.close()
         }
+    }
+
+    companion object {
+        private const val READ_CONTACTS_RC = 100
     }
 }
